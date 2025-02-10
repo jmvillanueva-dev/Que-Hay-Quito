750fc748034f350465f773f0ad7a1c792a60e1c6
Adrian-Caiza <adrian.caiza@epn.edu.ec>
2025-02-10 14:41:46 -0500

    Mis cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import TestFirestore from './services/api';   
 
 import Home from './components/Home';
 import LoginForm from './components/Login';
 import RegisterForm from './components/RegisterForm';
+import BlogPage from './pages/BlogPage'
+import BlogPage2 from './pages/BlogPage2'
 import './App.css';
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
 			<Route path="/Que-Hay-Quito" element={<Home />} />
 			<Route path="/Que-Hay-Quito/login" element={<LoginForm />} />
 			<Route path="/Que-Hay-Quito/register" element={<RegisterForm />} />
+			<Route path="/Que-Hay-Quito/blog" element={<BlogPage />} />
+			<Route path="/Que-Hay-Quito/blog2" element={<BlogPage2 />} />
 		</Routes>
 	);
 }
